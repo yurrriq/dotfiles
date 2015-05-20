968436b7306cea190f4c69c10065e62a0d05ae7a
Eric Bailey <eric@ericb.me>
2015-05-20 12:54:46 -0500

    updated thefuck config per https://github.com/nvbn/thefuck/wiki/Shell-aliases#fish

--- config.fish
+++ config.fish
@@ -72,8 +72,16 @@ function nw
 	~/Applications/nwjs-v0.12.0-osx-x64/nwjs.app/Contents/MacOS/nwjs $argv
 end
 
-function fuck
-  eval (thefuck $history[1])
+function __thefuck_repl -d 'Replace operators into fish-compatible'
+  set -l tmp (echo $argv | sed 's/ && / ; and /g')
+  echo $tmp | sed 's/ || / ; or /g'
+end
+
+function fuck -d 'Correct your previous console command'
+  set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+  thefuck $history[1] > $eval_script
+  eval (__thefuck_repl (cat $eval_script))
+  rm $eval_script
 end
 
 # OPAM configuration
