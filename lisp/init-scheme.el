12e91122b271e924a2ce8f2804a6c33377ffffac
Eric Bailey <eric@ericb.me>
2015-07-03 11:31:16 -0500

    scheme tweaks

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -6,4 +6,22 @@
             (put 'register-template 'scheme-indent-function 'defun)
             (put 'call-template 'scheme-indent-function 4)))
 
+(require-package 'geiser)
+
+(setq geiser-active-implementations '(guile))
+;; (setq geiser-repl-startup-time 10000)
+(setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
+(setq geiser-repl-query-on-kill-p nil)
+(setq geiser-implementations-alist
+      '(((dir "~/src/yurrriq/exercism/scheme")
+         guile)
+        ((regexp "\\.scm$")
+         guile)
+        ((regexp "\\.ss$")
+         racket)
+        ((regexp "\\.rkt$")
+         racket)))
+
+(global-set-key (kbd "C-c M-g") 'run-geiser)
+
 (provide 'init-scheme)
