5bf0e53484b7b174f8ecc60339cc1717e73675f8
Eric Bailey <eric@ericb.me>
2015-11-12 03:44:15 -0600

    Add init-dash-at-point.el

--- lisp/init-dash.el
+++
@@ -1,5 +0,0 @@
-(autoload 'dash-at-point "dash-at-point"
-  "Search the word at point with Dash." t nil)
-(global-set-key "\C-cd" 'dash-at-point)
-(global-set-key "\C-ce" 'dash-at-point-with-docset)
-alksdjlakj
