0af1ca1ebd7ff7431619701088f684b9a4c28269
Eric Bailey <eric@ericb.me>
2015-11-12 01:39:11 -0600

    Clean up init-erlang.el

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -1,9 +1,23 @@
+;;; init-erlang.el --- Customizations for writing Erlang
+
+;;; Commentary:
+
+;;; Code:
+
 (require-package 'erlang)
 
 (require 'erlang-start)
-(require 'edts-start)
+;; (require 'edts-start)
+
+(after-load 'erlang
+  (add-to-list 'auto-mode-alist '("\\.\\(e\\|h\\)rl?$"         . erlang-mode))
+  (add-to-list 'auto-mode-alist '("\\(sys\\|rebar\\)\\.config" . erlang-mode))
+  (add-to-list 'exec-path       "usr/local/lib/erlang/bin")
+  (setq erlang-indent-level 2)
+  (setq erlang-man-root-dir "/usr/local/lib/erlang/man")
+  (setq erlang-root-dir     "/usr/local/lib/erlang"))
 
 ;; (add-to-list 'ac-modes 'erlang-mode)
 
-
 (provide 'init-erlang)
+;;; init-erlang.el ends here
