03d8f100623bfe3468afbbffeafc02cfafd6431f
Eric Bailey <eric@ericb.me>
2015-11-12 02:36:40 -0600

    Update init-fonts.el
    Use 14pt Hack

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,18 +1,19 @@
-;; ===== DEFAULT FONT =====
+;;; init-fonts.el --- Default font setup
 
-(set-face-attribute 'default nil
-                    :family "Source Code Pro for Powerline"
-                    ;; :family "Office Code Pro"
-                    :height 140
-                    :weight 'regular
-                    :width 'normal)
+;;; Commentary:
 
+;;; Code:
 
-;; ===== CHANGING FONT SIZES =====
+(set-face-attribute
+ 'default nil                ; set default face on all current and future frames
+ :family  "Hack"             ; http://sourcefoundry.org/hack/
+ :height  140                ; 14pt
+ :weight 'normal
+ :width  'normal)
 
 (require-package 'default-text-scale)
 (global-set-key (kbd "C-M-=") 'default-text-scale-increase)
 (global-set-key (kbd "C-M--") 'default-text-scale-decrease)
 
-
 (provide 'init-fonts)
+;;; init-fonts.el ends here
