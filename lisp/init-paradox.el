a356fa6f6695e5079eb4e151527ebcade3df7474
Eric Bailey <eric@ericb.me>
2015-11-12 04:05:26 -0600

    Update init-paradox.el

--- lisp/init-paradox.el
+++ lisp/init-paradox.el
@@ -1,6 +1,16 @@
+;;; init-paradox.el --- A modernized package menu.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'init-elpa)
+
 (require-package 'paradox)
+(require 'paradox)
 
 ;; https://github.com/settings/tokens/new
-(setq paradox-github-token "TOKEN-GOES-HERE")
+;; (setq paradox-github-token "TOKEN-GOES-HERE")
 
 (provide 'init-paradox)
+;;; init-paradox.el ends here
