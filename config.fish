f93811a351f6a7fdc3af0f929db52e679607b08c
Eric Bailey <eric@ericb.me>
2015-11-12 04:28:10 -0600

    Update lots of stuff

--- config.fish
+++ config.fish
@@ -16,21 +16,27 @@ set fish_theme yurrriq
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-source ~/src/scripts/z.fish
+source ~/src/sjl/z-fish/z.fish
 
-set -x GOPATH $HOME/src/go
-set PATH $PATH ~/src/scripts $HOME/src/go
+set -x MONO_GAC_PREFIX "/usr/local"
 
-set PATH $PATH $HOME/Library/Haskell/bin
+# set -x GOPATH $HOME/src/go
+# set PATH $PATH ~/src/scripts $HOME/src/go
 
-source ~/src/erlang/17.5/activate.fish
+# set -x GHC_DOT_APP ~/Applications/ghc-7.10.2.app
+# set -x PATH $HOME/.cabal/bin $HOME/.local/bin {$GHC_DOT_APP}/Contents/bin $PATH
+set -x PATH $HOME/.cabal/bin $HOME/.local/bin $PATH
+
+# source ~/src/erlang/18.0/activate.fish
 
 # set LEIN_JAVA_CMD (which drip)
 set GPG_TTY (tty)
 
 set -x LEIN_FAST_TRAMPOLINE y
 
-set -x SLIMERJSLAUNCHER = ~/Applications/Firefox.app/Contents/MacOS/firefox
+# set -x SLIMERJSLAUNCHER = ~/Applications/Firefox.app/Contents/MacOS/firefox
+
+set -x MANPATH $MANPATH /usr/share/man /usr/local/share/man /usr/X11/share/man
 
 function cljsbuild
 	lein trampoline cljsbuild $argv
@@ -45,7 +51,7 @@ function k
 end
 
 function cloc
-	perl ~/src/scripts/cloc-1.62.pl $argv
+	perl ~/bin/cloc-1.64.pl $argv
 end
 
 function toggle_hidden_files
@@ -64,9 +70,23 @@ function agn
 	ag --nogroup $argv
 end
 
-function ec
-	emacsclient $argv
+function et
+  emacsclient -cnw -a "" $argv
 end
+
+
+function ec
+	emacsclient -cna "" $argv
+end
+
+function e
+  ec $argv
+end
+
+# set -x EDITOR "emacsclient -cnw -a ''"
+# set -x EDITOR "emacsclient -nw -a ''"
+set -x EDITOR 'emacsclient -cnw -a "" $argv'
+set -x VISUAL $EDITOR
 
 function nw
 	~/Applications/nwjs-v0.12.0-osx-x64/nwjs.app/Contents/MacOS/nwjs $argv
@@ -84,5 +104,29 @@ function fuck -d 'Correct your previous console command'
   rm $eval_script
 end
 
+function lalily
+  lilypond -I lalily -djob-count=8 -dmidi-extension=mid $argv
+end
+
 # OPAM configuration
-. /Users/yurrriq/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+# . /Users/mohacker/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+function rethinkdb-start
+  launchctl load /usr/local/opt/rethinkdb/homebrew.mxcl.rethinkdb.plist
+end
+
+function rethinkdb-stop
+  launchctl unload /usr/local/opt/rethinkdb/homebrew.mxcl.rethinkdb.plist
+end
+
+function elasticsearch-start
+  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+end
+
+function elasticsearch-stop
+  launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+end
+
+rvm default
+
+eval (docker-machine env default)
