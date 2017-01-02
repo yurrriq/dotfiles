90c0105883d7ddeac94a4942d5ed629b910cba0e
Eric Bailey <eric@ericb.me>
2017-01-02 15:08:56 -0600

    Add jq

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -32,7 +32,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = (with pkgs; [
     # Shell
-    fish autojump silver-searcher
+    fish autojump silver-searcher jq
     # Web
     curl httpie
     # Git
