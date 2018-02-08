a29e43217566f2f10d3692596a5c7c8dbb311086
Eric Bailey <eric@ericb.me>
2018-02-08 01:55:55 -0600

    profiles.clj: check in stale version

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,2 @@
-{:user { ;; :java-cmd "drip"
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [lein-ancient "0.6.8"]
-                  [lein-auto "0.1.2"]
-                  [lein-exec "0.3.5"]
-                  [lein-kibit "0.1.2"]
-                  [lein-bikeshed "0.2.0"]
-                  [jonase/eastwood "0.2.3"]
-                  [org.clojure/tools.nrepl "0.2.12"]
-                  [lein-typed "0.3.5"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]}
- :cuttle {:plugins [[lein-pprint "1.1.1"]]}}
+{:user {:plugins [[lein-auto "0.1.3"]]
+        :certificates ["clojars.pem"]}}
