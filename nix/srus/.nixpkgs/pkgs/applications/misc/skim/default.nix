cd5d9e5a9a4614c0927b9dd89838d121a03b8c38
Eric Bailey <e.bailey@sportradar.com>
2018-08-14 17:39:02 -0500

    skim: 1.4.29 -> 1.4.36

--- nix/srus/.nixpkgs/pkgs/applications/misc/skim/default.nix
+++ nix/srus/.nixpkgs/pkgs/applications/misc/skim/default.nix
@@ -2,26 +2,25 @@
 
 stdenv.mkDerivation rec {
   name = "skim-${version}";
-  version = "1.4.29";
+  version = "1.4.36";
 
   src = fetchurl {
     url = "https://downloads.sourceforge.net/skim-app/Skim/Skim-${version}/Skim-${version}.dmg";
-    sha256 = "0lg0cqvjdngdwr7dvfndpv8d0f9wwr2kppvbc76bb6pkl0whvh34";
+    sha256 = "0smg8ldr7c90gakqg20f8nrq94ak80kkk4456y2abxjk6jijmhr3";
   };
 
   buildInputs = [ undmg ];
 
   installPhase = ''
-    local app=$out/Applications/Skim.app
-    mkdir -p $app
-    cp -R . $app
-    chmod a+x $app/Contents/MacOS/Skim
+    install -dm755 "$out/Applications/Skim.app"
+    cp -R . "$_"
+    chmod a+x "$_/Contents/MacOS/Skim"
   '';
 
   meta = with stdenv.lib; {
     description = "PDF reader and note-taker for OS X";
-    homepage = "http://skim-app.sourceforge.net";
-    repositories.svn = "https://svn.code.sf.net/p/skim-app/code/trunk";
+    homepage = "https://skim-app.sourceforge.io";
+    repositories.svn = "http://svn.code.sf.net/p/skim-app/code/trunk";
     license = licenses.bsd2;
     platforms = platforms.darwin;
     maintainers = with maintainers; [ yurrriq ];
