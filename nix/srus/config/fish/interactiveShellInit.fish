206bce21612426a35c5a43aea7ffb9bc6cf257e9
Eric Bailey <e.bailey@sportradar.com>
2018-08-27 09:36:49 -0500

    [fish/srus] Add initial kcexec function

--- nix/srus/config/fish/interactiveShellInit.fish
+++ nix/srus/config/fish/interactiveShellInit.fish
@@ -8,4 +8,16 @@ function hicat -d 'Hackish hicat clone via pygments'
 end
 
 
+function kcexec
+    argparse -N 2 --name=kcexec 'r/replica=' -- $argv
+    or return
+
+    if not set -q _flag_replica
+        set -l _flag_replica 0
+    end
+
+    kubectl exec -it (kubectl get pod -o jsonpath="{.items[$_flag_replica].metadata.name}" -l app=$argv[1]) -c $argv[1] $argv[2..-1]
+end
+
+
 rvm default
