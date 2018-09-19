35c766cf213fe69c3a7ebfe53405c5e546b60e43
Eric Bailey <e.bailey@sportradar.com>
2018-09-19 12:18:28 -0500

    emacs: update custom.el

--- spacemacs/srus/.emacs.d/private/local/custom.el
+++ spacemacs/srus/.emacs.d/private/local/custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values
+   (quote
+    ((org-src-preserve-indentation . t)
+     (org-src-fontify-natively . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C")))))
+ )
