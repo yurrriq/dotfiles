3b03a5e5aef1ae4999c7aecd74b6b88d29e8d44f
Eric Bailey <e.bailey@sportradar.com>
2018-10-29 11:24:57 -0500

    Update Emacs config

--- spacemacs/srus/.emacs.d/private/local/custom.el
+++ spacemacs/srus/.emacs.d/private/local/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-hl-todo-mode t)
+ '(hl-todo-activate-in-modes (quote (prog-mode yaml-mode)))
  '(safe-local-variable-values
    (quote
     ((org-src-preserve-indentation . t)
