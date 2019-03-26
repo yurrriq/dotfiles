0a1a4776a866e9ea09aef42afe9a62959c1221c2
Eric Bailey <e.bailey@sportradar.com>
2019-03-26 15:43:51 -0500

    [emacs] Add note re: org-mode hack

--- emacs/common/.emacs.d/private/local/custom.el
+++ emacs/common/.emacs.d/private/local/custom.el
@@ -15,3 +15,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; TODO: (add-to-list 'org-structure-template-alist '("n" "#+NAME: ?"))
