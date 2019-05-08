7aef26d6d72e635d168688165f72e4f2f9b309d6
Eric Bailey <e.bailey@sportradar.com>
2019-05-08 16:20:35 -0500

    [emacs] No more tabs

--- emacs/common/.emacs.d/private/local/custom.el
+++ emacs/common/.emacs.d/private/local/custom.el
@@ -8,7 +8,8 @@
  '(safe-local-variable-values
    (quote
     ((org-src-preserve-indentation . t)
-     (org-src-fontify-natively . t)))))
+     (org-src-fontify-natively . t))))
+ '(indent-tabs-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
