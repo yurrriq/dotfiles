30d5dd56676374342124796dc771f58e84c22983
Eric Bailey <e.bailey@sportradar.com>
2019-05-23 14:37:17 +0200

    [nix] Rework k8s envs

--- nix/.config/nixpkgs/overlays/50-envs.nix
+++ nix/.config/nixpkgs/overlays/50-envs.nix
@@ -58,13 +58,15 @@ self: super: rec {
     ]));
   };
 
-  k8s-0 = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
-    pkgs = _nur.repos.yurrriq.lib.pinnedNixpkgs rec {
+  k8s-stable = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.lib.buildK8sEnv {
+    pkgs = import (_nur.repos.yurrriq.lib.fetchNixpkgs {
       rev = "e20ee8a710f3a8ea378bb664c2dbfa32dcf399a7";
       sha256 = "0h063hhywrb4vj9g1lg9dp0r9h5i8b5n923iminnckkxxbr3iap1";
+    }) {
+      overlays = [ ((import <nur> {}).repos.yurrriq.overlays.nur) ];
     };
 
-    name = "k8s-0";
+    name = "k8s-stable";
     config = {
       k8s = {
         version = "1.11.7";
@@ -76,40 +78,21 @@ self: super: rec {
       };
       helm = {
         flavor = "darwin-amd64";
-        version = "2.12.3";
-        sha256 = "0lcnmwqpf5wwq0iw81nlk5fpj4j5p4r6zkrjvbqw5mrjacpa9qf9";
-      };
-      helmfile = {
-        version = "0.54.0";
-        sha256 = "0x0kh1dshyygsh22sd5ncbimqx3sl3vn1pdr0spzhfy628rg7lax";
-      };
-    };
-  };
-
-  k8s-1 = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
-    pkgs = _nur.repos.yurrriq.lib.pinnedNixpkgs {
-      rev = "e20ee8a710f3a8ea378bb664c2dbfa32dcf399a7";
-      sha256 = "0h063hhywrb4vj9g1lg9dp0r9h5i8b5n923iminnckkxxbr3iap1";
-    };
-
-    name = "k8s-1";
-    config = k8s-0.config // {
-      helm = {
-        flavor = "darwin-amd64";
         version = "2.13.1";
         sha256 = "0a21xigcblhc9wikl7ilqvs7514ds4x71jz4yv2kvv1zjvdd9i8n";
       };
+      helmfile = {
+        version = "0.64.1";
+        sha256 = "1258c545fv4mcrzaw3z5gxl264fcahigaijgkjd4igh4pl0z0wxk";
+      };
     };
   };
 
-  k8s-2 = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
-    pkgs = _nur.repos.yurrriq.lib.pinnedNixpkgs {
-      rev = "e20ee8a710f3a8ea378bb664c2dbfa32dcf399a7";
-      sha256 = "0h063hhywrb4vj9g1lg9dp0r9h5i8b5n923iminnckkxxbr3iap1";
-    };
+  k8s-legacy = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
+    inherit (k8s-stable) pkgs;
 
-    name = "k8s-2";
-    config = k8s-0.config // {
+    name = "k8s-legacy";
+    config = k8s-stable.config // {
       helm = {
         flavor = "darwin-amd64";
         version = "2.11.0";
@@ -118,14 +101,23 @@ self: super: rec {
     };
   };
 
-  k8s-3 = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
-    inherit (k8s-1) pkgs;
+  k8s-dev = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
+    inherit (k8s-stable) pkgs;
 
-    name = "k8s-3";
-    config = k8s-1.config // {
+    name = "k8s-next";
+    config = k8s-stable.config // {
+      helm = {
+        flavor = "darwin-amd64";
+        version = "2.14.0";
+        sha256 = "1cxmmi7pwp0qrv34ini8gklf9kvqal778q38bkacdlqrnfj0zx05";
+      };
       k8s = {
         version = "1.11.9";
         sha256 = "0v2d7dg5iw339jxr8dn5jm0kpdqcl82agqgaq8r80rvdl4yi6rwq";
+      };
+      kops = {
+        version = "1.12.1";
+        sha256 = "0jia8dhawh786grnbpn64hvsdm6wz5p7hqir01q5xxpd1psnzygj";
       };
     };
   };
