f2bef08208fbcea75268aa68f095b2a45adc8140
Eric Bailey <eric@ericb.me>
2019-08-28 08:50:27 -0500

    [nix] Update k8s envs

--- nix/.config/nixpkgs/overlays/50-envs.nix
+++ nix/.config/nixpkgs/overlays/50-envs.nix
@@ -65,44 +65,6 @@ self: super: rec {
     ]));
   };
 
-  k8s-stable = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.lib.buildK8sEnv {
-    pkgs = import (_nur.repos.yurrriq.lib.fetchNixpkgs {
-      rev = "2e869f71a7aebf0e96114cf7317b73583e893eac";
-      sha256 = "07kzrsn1lcdkagnqxkkfdr6migd5nbwb8k9jcz20wmbg8xc6n748";
-    }) {
-      overlays = [ ((import <nur> {}).repos.yurrriq.overlays.nur) ];
-    };
-
-    name = "k8s-stable";
-    config = {
-      eksctl = {
-        version = "0.1.38";
-        sha256 = "1nhsy4d1a1vh7g2ibcxnzgxnldfyh51hiq4v4vy123487b6ndqd0";
-      };
-      k8s = {
-        version = "1.11.7"; # TODO: 1.11.10
-        sha256 = "03dq9p6nwkisd80f0r3sp82vqx2ac4ja6b2s55k1l8k89snfxavf";
-      };
-      kops = {
-        version = "1.11.1"; # TODO: 1.12.2
-        sha256 = "0jia8dhawh786grnbpn64hvsdm6wz5p7hqir01q5xxpd1psnzygj";
-      };
-      helm = {
-        version = "2.13.1";
-        flavor = "linux-amd64";
-        sha256 = "1wyhyxsm7260wjx9lqzg7vhply52m9yb5mcixifx0q4lq3s2pgp4";
-      };
-      helmfile = {
-        # TODO
-        # version = "0.79.3";
-        # sha256 = "0wgfpidpqyvh41dnw351v91z4szi1s6lqak9li2pmddz1rdkx66v";
-        version = "0.73.3";
-        sha256 = "1br9d0fq5dncvz6ypzwflr7bscyjryvyqjyf7jvyf36ibl8x8gy8";
-        modSha256 = "1ksz1c4j7mhsbq6ifqab04588d48c9glyhr4d3d4jyvi19qhwx1d";
-      };
-    };
-  };
-
   k8s-legacy = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.pkgs.buildK8sEnv {
     inherit (k8s-stable) pkgs;
 
@@ -110,14 +72,51 @@ self: super: rec {
     config = k8s-stable.config // {
       helm = {
         flavor = "linux-amd64";
-        version = "2.11.0";
-        sha256 = "14ap6wm7g9fr1yjx6n382rmnhyn5rf12rxmv7dyypcd2h92cjm8a";
+        # version = "2.11.0";
+        # sha256 = "14ap6wm7g9fr1yjx6n382rmnhyn5rf12rxmv7dyypcd2h92cjm8a";
+        version = "2.12.1";
+        sha256 = "03rsbdbxxn1hfh0pkqjgm68dyvyczxa7msqky9s748afjg9yfkjv";
       };
       kops = {
         version = "1.11.0";
         sha256 = "1z67jl66g79q6v5kjy9qxx2xp656ybv5hrc10h3wmzy0b0n30s4n";
       };
+    };
+  };
 
+  k8s-stable = let _nur = import <nur> { pkgs = self; }; in _nur.repos.yurrriq.lib.buildK8sEnv {
+    pkgs = import (_nur.repos.yurrriq.lib.fetchNixpkgs {
+      rev = "99f600917fa6bdea714ae27d40467affd37c0169";
+      sha256 = "0p6gaz397x4hplwnywxg5538pqk9p9nls72minrmc306ly07jpf6";
+    }) {
+      overlays = [ ((import <nur> {}).repos.yurrriq.overlays.nur) ];
+    };
+
+    name = "k8s-stable";
+    config = {
+      eksctl = {
+        version = "0.3.0";
+        sha256 = "0xbvx3rpd97hc49njvc4mx5zm2x08hrg8kpkapa8qigyajjxz9fg";
+      };
+      k8s = {
+        version = "1.11.7"; # TODO: 1.13.10
+        sha256 = "03dq9p6nwkisd80f0r3sp82vqx2ac4ja6b2s55k1l8k89snfxavf";
+      };
+      kops = {
+        version = "1.11.1"; # TODO: 1.13.0
+        sha256 = "0jia8dhawh786grnbpn64hvsdm6wz5p7hqir01q5xxpd1psnzygj";
+      };
+      helm = {
+        version = "2.13.1"; # TODO: 2.14.3
+        flavor = "linux-amd64";
+        sha256 = "1wyhyxsm7260wjx9lqzg7vhply52m9yb5mcixifx0q4lq3s2pgp4";
+      };
+      # FIXME
+      helmfile = {
+        version = "0.79.3";
+        sha256 = "0wgfpidpqyvh41dnw351v91z4szi1s6lqak9li2pmddz1rdkx66v";
+        modSha256 = "0jqss8bgwvggygmr5y2yj98apkqs8z3vmwyfh2f6s67k68h57m57";
+      };
     };
   };
 
@@ -128,12 +127,16 @@ self: super: rec {
     config = k8s-stable.config // {
       helm = {
         flavor = "linux-amd64";
-        version = "2.14.2";
-        sha256 = "0crnn39wip0ysg8z0084mjjhfyw5cqg7mlgxv5g6g6j14gqnmq2j";
+        version = "2.14.3";
+        sha256 = "03rad3v9z1kk7j9wl8fh0wvsn46rny096wjq3xbyyr8slwskxg5y";
       };
       k8s = {
-        version = "1.11.9"; # TODO: 1.11.10
-        sha256 = "0v2d7dg5iw339jxr8dn5jm0kpdqcl82agqgaq8r80rvdl4yi6rwq";
+        version = "1.12.9";
+        sha256 = "0fxrvwl60wmj30azaw5ws8ilm62fmabz1l9ixpxq3lbrmkzhvx0m";
+      };
+      kops = {
+        version = "1.13.0";
+        sha256 = "04kbbg3gqzwzzzq1lmnpw2gqky3pfwfk7pc0laxv2yssk9wac5k1";
       };
     };
   };
@@ -144,15 +147,8 @@ self: super: rec {
     name = "k8s-next";
     config = k8s-dev.config // {
       k8s = {
-        # TODO
-        # version = "1.12.9";
-        # sha256 = "0fxrvwl60wmj30azaw5ws8ilm62fmabz1l9ixpxq3lbrmkzhvx0m";
-        version = "1.12.8";
-        sha256 = "164g1i3cvdk39n294c9i1vy633xisxlcr4qb57p0q1px1fsbs50c";
-      };
-      kops = {
-        version = "1.12.2";
-        sha256 = "0937crwifnld7r5pf5gvab9ibmf8k44dafr9y3hld2p01ijrari1";
+        version = "1.13.10";
+        sha256 = "0x9blsc0pn77aiaaws45r1531jl29psgljq0l67kpw0bgppg5y7w";
       };
     };
   };
