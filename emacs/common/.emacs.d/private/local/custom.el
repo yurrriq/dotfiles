dac74684dfb85a7e014f17033ef652bdbf43d076
Eric Bailey <eric@ericb.me>
2019-11-18 00:41:52 -0600

    [emacs] Set GAP executable

--- emacs/common/.emacs.d/private/local/custom.el
+++ emacs/common/.emacs.d/private/local/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gap-executable "gap")
  '(global-hl-todo-mode t)
  '(hl-todo-activate-in-modes (quote (prog-mode text-mode yaml-mode)))
  '(indent-tabs-mode nil)
