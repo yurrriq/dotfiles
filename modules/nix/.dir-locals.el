5535f63eddb753502ae64d9b9d6be66a1657ef4b
Eric Bailey <eric@ericb.me>
2020-01-21 11:56:15 -0600

    Housekeeping

--- modules/nix/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((js-mode
-  (js-indent-level . 4)))
--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -2,15 +2,6 @@
 
 let
 
-  fetchTarballFromGitHub =
-    { owner, repo, rev, sha256, ... }:
-    builtins.fetchTarball {
-      url = "https://github.com/${owner}/${repo}/tarball/${rev}";
-      inherit sha256;
-    };
-
-  fromJSONFile = f: builtins.fromJSON (builtins.readFile f);
-
   seemsDarwin = null != builtins.match ".*darwin$" builtins.currentSystem;
 
 in
