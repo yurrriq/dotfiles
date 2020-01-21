82b0e7d8a3c1d5633c29841718ce9cd0d1cee88a
Eric Bailey <eric@ericb.me>
2020-01-21 02:24:57 -0600

    [fish] Adopt clone and latest

--- config/fish/interactiveShellInit.fish
+++ config/fish/interactiveShellInit.fish
@@ -1,3 +1,48 @@
+function clone
+  function __update
+    test -d $argv[1]; and cd $argv[1]; and git fetch --all; and git pull
+  end
+
+  function __usage
+    echo "Usage: clone [username] [repository] [[destination]]"
+  end
+
+  set --local num_args (count $argv)
+
+  if test $num_args -ge 2
+    set --local user $argv[1]
+    set --local repo $argv[2]
+
+    if test $num_args -eq 2
+      set dest ~/src/$user/$repo
+    else if test $num_args -eq 3
+      set dest $argv[3]/$user/$repo
+    else
+      __usage
+    end
+
+    echo $dest
+
+    git clone git@github.com:$user/$repo.git $dest; or __update $dest; or __usage
+  else
+    __usage
+  end
+end
+
+
+function latest -d 'Print the latest tag (on GitHub) for a given user and repo.'
+  # TODO: __usage
+
+  set --local num_args (count $argv)
+
+  if test $num_args -eq 2
+    set --local user $argv[1]
+    set --local repo $argv[2]
+    http https://api.github.com/repos/$user/$repo/tags | jq '.[0].name'
+  end
+end
+
+
 command -sq fluidsynth; and function playmidi
     fluidsynth -i ~/lib/arachno-soundfont/Arachno\ SoundFont\ -\ Version\ 1.0.sf2 $argv
 end
@@ -68,12 +113,6 @@ command -sq kubectl; and begin
         command helmfile --version
         printf "kops %s\n" (command kops version)
     end
-end
-
-
-function vpn-restart
-    sudo launchctl stop me.ericb.openconnect
-    and sudo launchctl start me.ericb.openconnect
 end
 
 
