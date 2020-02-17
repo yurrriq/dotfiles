b13920cc5dbf879e0fa668f32b17b26dac4821d0
Eric Bailey <eric@ericb.me>
2020-02-16 22:48:03 -0600

    Squashed 'noweb-minted/' content from commit 75a58ba
    git-subtree-dir: noweb-minted
    git-subtree-split: 75a58badd98eee00b57224f20435a8b5d8dcd0b3

---
+++ guessPygmentizeLexer.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from pygments.lexers import guess_lexer, guess_lexer_for_filename, get_lexer_for_filename
+import sys
+
+try:
+    filename = "FOO." + str(sys.argv[1])
+    print get_lexer_for_filename(filename).name
+except:
+    print "text"
