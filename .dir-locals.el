fbadfb525f58631bf973d0fd50483268eee5e354
Eric Bailey <eric@ericb.me>
2020-03-09 01:06:54 -0500

    .dir-locals.el: init, fix noweb/emojify conflict

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+(("src"
+  . ((nil . ((eval
+              . (progn (global-emojify-mode 0)
+                       (global-emojify-mode-line-mode 0))))))))
