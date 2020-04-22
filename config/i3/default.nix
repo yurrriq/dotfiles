0f1a24fcb9f310883fea347c5e0aa7e86019c04c
Eric Bailey <eric@ericb.me>
2020-04-22 11:03:44 -0500

    i3: start volumeicon

--- config/i3/default.nix
+++ config/i3/default.nix
@@ -110,6 +110,12 @@
             "Print" = "exec flameshot gui";
           };
           inherit modifier;
+          startup = [
+            {
+              command = "volumeicon";
+              notification = false;
+            }
+          ];
         };
   };
 
