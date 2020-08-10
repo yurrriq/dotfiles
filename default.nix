75c8a5072ddf188fd6b116926b94ba1377aecf35
Eric Bailey <eric@ericb.me>
2020-08-10 17:32:44 -0500

    default.nix: remove obsolete configurePhase

--- default.nix
+++ default.nix
@@ -20,13 +20,6 @@ pkgs.stdenv.mkDerivation rec {
     xelatex-noweb
   ];
 
-  configurePhase = ''
-    sed -i '0,/-S gawk -f/s//gawk/' ./bin/fix-underscores
-    patchShebangs ./bin/fix-underscores ./noweb-minted/
-    sed -i '0,/gawk$/s//gawk -f/' ./bin/fix-underscores
-    export PATH=$PWD/bin:$PWD/noweb-minted:$PATH
-  '';
-
   makeFlags = [
     "PREFIX=${placeholder "out"}"
   ];
