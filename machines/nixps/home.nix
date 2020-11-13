9a9a9614e9bedf887b796456540029412b15863b
Eric Bailey <eric@ericb.me>
2020-11-13 00:00:47 -0600

    [nixps] Rework some more

--- machines/nixps/home.nix
+++ machines/nixps/home.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, ... }:
 
 {
-  imports = (import <nurpkgs> { }).home-manager.modules ++ [
+  imports = [
     ../../config/bat.nix
     ../../config/browserpass.nix
     ../../config/direnv.nix
@@ -23,7 +23,6 @@
     ../../config/xmonad
     # TODO: ../../config/vim.nix
   ];
-
   accounts.email.accounts.primary = {
     address = "eric@ericb.me";
     gpg.key = "F88372B24A806FF23BCB3A4E2DDDF8606958B3F9";
@@ -31,36 +30,19 @@
     primary = true;
     realName = "Eric Bailey";
   };
-
   home.packages = with pkgs; [
-    carla
+    exa
+    fd
+
+    # carla
     clementine
-    gnucash
     reaper
+
     steam
-    tomb
+
     file
     lsof
-    fd
-    exa
+    tomb
   ];
-
   _module.args.pkgs = lib.mkForce pkgs;
-
-  programs.starship = {
-    enable = true;
-    package = nixpkgs-unstable.starship;
-    settings = {
-      add_newline = false;
-      character.symbol = "λ";
-      # line_break.disabled = true;
-      nix_shell = {
-        impure_msg = ""; # "😈";
-        # pure_msg = "👼";
-      };
-      stats.disabled = false;
-      time.disabled = false;
-
-    };
-  };
 }
