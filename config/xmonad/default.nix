ea9aadbf4c78b22fc41b1578cad6696bb095dbe0
Eric Bailey <e.bailey@sportradar.com>
2021-03-02 21:27:47 -0600

    xmonad: clean up config

--- config/xmonad/default.nix
+++ config/xmonad/default.nix
@@ -21,32 +21,7 @@
     theme = "purple";
   };
 
-  services.network-manager-applet.enable = true;
-
-  services.pasystray.enable = true;
-
   services.picom.enable = true;
-
-  services.stalonetray = {
-    enable = false;
-    config = {
-      background = "#001e26";
-      decorations = "none";
-      dockapp_mode = "none";
-      geometry = "5x1-860-0";
-      grow_gravity = "NE";
-      icon_gravity = "NE";
-      icon_size = 43;
-      kludges = "force_icons_size";
-      no_shrink = false;
-      skip_taskbar = true;
-      sticky = true;
-      transparent = false;
-      window_layer = "bottom";
-      window_strut = "none";
-      window_type = "dock";
-    };
-  };
 
   xdg.configFile."xmobar/xmobarrc".source = ../xmobar/xmobarrc;
 
