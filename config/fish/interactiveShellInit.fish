3bf2b0ae9ebc27eb65869ce0f3cce7547b1a4a8e
Eric Bailey <eric@ericb.me>
2021-03-11 22:13:30 -0600

    fish: tweak latest function

--- config/fish/interactiveShellInit.fish
+++ config/fish/interactiveShellInit.fish
@@ -28,7 +28,7 @@ function clone
     __usage
   end
 end
-function latest -d 'Print the latest tag (on GitHub) for a given user and repo.'
+function latest -d 'Print the latest release (on GitHub) for a given user and repo.'
   # TODO: __usage
 
   set --local num_args (count $argv)
@@ -36,7 +36,7 @@ function latest -d 'Print the latest tag (on GitHub) for a given user and repo.'
   if test $num_args -eq 2
     set --local user $argv[1]
     set --local repo $argv[2]
-    http https://api.github.com/repos/$user/$repo/tags | jq '.[0].name'
+    http https://api.github.com/repos/$user/$repo/releases/latest | jq -r '.tag_name'
   end
 end
 command -sq aws; and command -sq jq; and \
