5f48243219109b01e425712e5e703495afbec9b7
Eric Bailey <eric@ericb.me>
2021-03-12 00:12:35 -0600

    htop: configure

--- config/htop.nix
+++ config/htop.nix
@@ -2,6 +2,12 @@
 
 {
 
-  programs.htop.enable = true;
+  programs.htop = {
+    enable = true;
+    colorScheme = 6;
+    cpuCountFromZero = true;
+    showCpuUsage = true;
+    highlightBaseName = true;
+  };
 
 }
