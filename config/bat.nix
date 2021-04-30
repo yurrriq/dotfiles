b11fb5656b7491c3b84e4382ca776c7a28577217
Eric Bailey <eric@ericb.me>
2021-04-30 16:26:14 -0500

    bat: tweak config

--- config/bat.nix
+++ config/bat.nix
@@ -5,6 +5,7 @@
   programs.bat = {
     enable = true;
     config = {
+      map-syntax = "*.yaml.gotmpl:YAML";
       pager = "less -FR";
       style = "changes";
       theme = "Monokai Extended";
