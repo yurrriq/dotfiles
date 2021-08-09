fc23c7b330b731af2f355b1c30fdc89692149181
Eric Bailey <eric@ericb.me>
2021-08-08 23:45:27 -0500

    [yara] Initial commit

---
+++ machines/yara/configuration.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/virtualisation/amazon-image.nix> ];
+
+  ec2.hvm = true;
+
+  environment.systemPackages = with pkgs; [
+    bashInteractive
+    git
+    httpie
+  ];
+
+}
