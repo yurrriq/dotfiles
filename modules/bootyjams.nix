a30c7b6f8eba24417592673eee4f635b24416033
Eric Bailey <eric@ericb.me>
2022-04-15 19:22:14 -0500

    bootyjams: add /mnt/reaper

--- modules/bootyjams.nix
+++ modules/bootyjams.nix
@@ -6,19 +6,26 @@ in
   environment.systemPackages = with pkgs; [
     nfs-utils
   ];
-  fileSystems."/mnt/music" = {
-    device = "//192.168.1.147/homes/eric/music";
-    fsType = "cifs";
-    options = [
-      "credentials=/etc/nixos/secrets/bootyjams.club"
-      "gid=${toString config.ids.gids.users}"
-      "noauto"
-      "rw"
-      "uid=${toString config.users.users.${username}.uid}"
-      "vers=2.0"
-      # "x-systemd.device-timeout=5s"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.mount-timeout=5s"
-    ];
-  };
+  fileSystems =
+    let
+      mkMount = path: {
+        device = "//192.168.1.147${path}";
+        fsType = "cifs";
+        options = [
+          "credentials=/etc/nixos/secrets/bootyjams.club"
+          "gid=${toString config.ids.gids.users}"
+          "noauto"
+          "rw"
+          "uid=${toString config.users.users.${username}.uid}"
+          "vers=2.0"
+          # "x-systemd.device-timeout=5s"
+          "x-systemd.idle-timeout=600"
+          "x-systemd.mount-timeout=5s"
+        ];
+      };
+    in
+    {
+      "/mnt/music" = mkMount "/homes/eric/music";
+      "/mnt/reaper" = mkMount "/REAPER Media";
+    };
 }
