10d6c7cb06f6192e4593331c4047e8bee5ba59b4
Eric Bailey <eric@ericb.me>
2023-07-14 18:03:42 -0500

    fix(fish): drop -G from ll

--- config/fish/aliases.nix
+++ config/fish/aliases.nix
@@ -6,7 +6,7 @@
     j = "z";
     k = "clear";
     l = "exa --color=auto -G";
-    ll = "exa --color=auto -Gla";
+    ll = "exa --color=auto -la";
     pbcopy = "xclip -sel clipboard";
     pbpaste = "xclip -sel clipboard -o";
   };
