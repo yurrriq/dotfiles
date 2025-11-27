bf776b6a88a69d8e603312f32c139d1afd16f6a8
Eric Bailey <eric@ericb.me>
2025-11-26 22:01:43 -0600

    build: comment out .dir-locals.el for now

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,22 +1,22 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (eval progn
-        (direnv-update-environment)
-        (rainbow-mode 1)))
- ("config/xmobar"
-  (nil
-   (mode . haskell)
-   (eval progn
-         (emojify-mode 1))))
- ("config/xmonad"
-  (nil
-   (mode . interactive-haskell)
-   (eval progn
-         (emojify-mode 1))))
- ("src"
-  (nil
-   (eval progn
-         (global-emojify-mode 0)
-         (global-emojify-mode-line-mode 0)))))
+;; ((nil
+;;   (eval progn
+;;         (direnv-update-environment)
+;;         (rainbow-mode 1)))
+;;  ("config/xmobar"
+;;   (nil
+;;    (mode . haskell)
+;;    (eval progn
+;;          (emojify-mode 1))))
+;;  ("config/xmonad"
+;;   (nil
+;;    (mode . interactive-haskell)
+;;    (eval progn
+;;          (emojify-mode 1))))
+;;  ("src"
+;;   (nil
+;;    (eval progn
+;;          (global-emojify-mode 0)
+;;          (global-emojify-mode-line-mode 0)))))
